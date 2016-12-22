0d669e7a1be91775753efb6f060807ee99015014
David Healy <DHealy@jwprince.com.au>
2016-12-22 13:30:02 +1100

    Customer Model updated

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -7,5 +7,11 @@ namespace Vidly.Models
 {
     public class Customer
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsSubscribedToNewsLetter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
+        public DateTime? Birthdate { get; set; }
     }
 }
